b55704b468b8423ca4857bb36ee19df8165a4a6d
Yashu Patel <148859965+YashuPatel1724@users.noreply.github.com>
2024-12-02 14:57:25 +0530

    Update Q5.dart

--- oop exam/Q5.dart
+++ oop exam/Q5.dart
@@ -12,21 +12,27 @@ abstract class Employee {
     name = stdin.readLineSync() ?? '';
 
     print('Enter Salary (as an integer):');
-    int.parse(stdin.readLineSync() ?? '') ?? 0;
+    salary = int.parse(stdin.readLineSync() ?? '') ?? 0;
+  }
+
+  void get()
+  {
+    print(name);
+     print(salary);
   }
 }
 
 class Manager extends Employee {
   @override
   double calculateBonus() {
-    return salary * 0.2; 
+    return salary + 1000; 
   }
 }
 
 class Developer extends Employee {
   @override
   double calculateBonus() {
-    return salary * 0.1; 
+    return salary + 500; 
   }
 }
 
@@ -36,9 +42,11 @@ void main() {
 
   print('Enter details for Manager:');
   manager.inputDetails();
+  manager.get();
 
   print('Enter details for Developer:');
   developer.inputDetails();
+  developer.get();
 
   print('Manager Bonus for ${manager.name}: ${manager.calculateBonus()}');
   print('Developer Bonus for ${developer.name}: ${developer.calculateBonus()}');
